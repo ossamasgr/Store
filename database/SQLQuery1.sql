822ed89872ed085563635c1c254356056c11e8e6
Ossama Assaghir <oassaghir@octopeek.com>
2021-12-01 12:34:36 +0100

    database

---
+++ database/SQLQuery1.sql
@@ -0,0 +1,25 @@
+create database store
+use store
+create table users(
+id varchar(100) primary key,
+full_name varchar(100),
+password varchar(100),
+type varchar(100)
+)
+create table products(
+id int primary key,
+name varchar(100),
+price float,
+)
+create table storage(
+id int foreign key references products,
+qte_in_stock int,
+last_updated date
+)
+create table command(
+order_id int primary key, 
+id int foreign key references products,
+qte int,
+o_date date,
+user_id varchar(100) foreign key references users
+)
